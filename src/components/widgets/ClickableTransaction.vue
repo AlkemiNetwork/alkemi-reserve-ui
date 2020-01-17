f26fa4d5d33e9f7ae059254ff672eab8befe346a
Lieu Nguyen <lieu@alkemi.ai>
2020-01-17 22:45:09 +0700

    click intiate wallet

--- src/components/widgets/ClickableTransaction.vue
+++ src/components/widgets/ClickableTransaction.vue
@@ -1,35 +1,39 @@
 <template>
   <span v-if="transaction" class="small">
-     <a class="token-id" :href="buildLink" target="_blank">[view transaction]</a>
+    <a class="token-id" :href="buildLink" target="_blank">[view transaction]</a>
   </span>
 </template>
 
 <script>
-  import { mapGetters, mapState } from 'vuex';
-  export default {
-    name: 'clickableTransaction',
-    components: {},
-    props: {
-      transaction: {
-        type: String
-      }
-    },
-    computed: {
-        ...mapState("ContractController", [
-          "etherscanBase"
-        ]),
-      dotDotDot: function () {
-        if (this.transaction) {
-          return this.transaction.substr(0, 6) + '...' + this.transaction.substr(this.transaction.length - 6, this.transaction.length);
-        }
-        return '';
-      },
-      buildLink: function () {
-        return `${this.etherscanBase}/tx/${this.transaction}`;
-      }
+import { mapGetters, mapState } from "vuex";
+export default {
+  name: "clickableTransaction",
+  components: {},
+  props: {
+    transaction: {
+      type: String
     }
-  };
+  },
+  computed: {
+    ...mapState("ContractController", ["etherscanBase"]),
+    dotDotDot: function() {
+      if (this.transaction) {
+        return (
+          this.transaction.substr(0, 6) +
+          "..." +
+          this.transaction.substr(
+            this.transaction.length - 6,
+            this.transaction.length
+          )
+        );
+      }
+      return "";
+    },
+    buildLink: function() {
+      return `${this.etherscanBase}/tx/${this.transaction}`;
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
